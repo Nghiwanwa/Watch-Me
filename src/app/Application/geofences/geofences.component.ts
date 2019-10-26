fc6921199bbb4f958c4283065a25945c2cd47d11
unknown <paulaipawa@gmail.com>
2019-10-26 04:35:25 -0700

    Made some changes to the children's component

---
+++ src/app/Application/geofences/geofences.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-geofences',
+  templateUrl: './geofences.component.html',
+  styleUrls: ['./geofences.component.css']
+})
+export class GeofencesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
